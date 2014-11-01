7db5393a3a6c7008d96f1341af548faa04cf598f
patmood <patrick.n.moody@gmail.com>
2014-11-01 13:19:19 -0700

    punch kick

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,8 +18,41 @@ window.onload = function () {
 
 function Play() {}
 Play.prototype = {
-  create: function() {
-    console.log('play state!!!')
+  preload: function() {
+    this.game.load.spritesheet('dude', 'images/tank_guy.png', 130, 160)
+    this.game.load.image('button', 'images/button_green.png')
+    this.sequence = []
+  }
+, create: function() {
+    this.game.stage.backgroundColor = '#182d3b'
+    this.dude = this.game.add.sprite(300, 200, 'dude')
+    var punchButton = this.game.add.button(200, 400, 'button', this.move, this)
+    punchButton.name = 'punch'
+    punchButton.frameNum = 1
+
+    var kickButton = this.game.add.button(450, 400, 'button', this.move, this)
+    kickButton.name = 'kick'
+    kickButton.frameNum = 2
+  }
+, update: function() {
+
+  }
+, checkEnd: function() {
+    if (this.sequence.length >= 5) {
+      console.log('game over, man game over')
+      console.log(this.sequence)
+    }
+  }
+, move: function(item) {
+    var _this = this
+    debugger
+    this.dude.frame = item.frameNum
+    this.sequence.push(item.name)
+    this.checkEnd()
+    setTimeout(function() {
+      _this.dude.frame = 0
+    }
+    , 200)
   }
 }
 
