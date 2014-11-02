b7e1ba492a5c1c6c2651cc4df189d186dbe9e984
patmood <patrick.n.moody@gmail.com>
2014-11-02 12:57:12 -0800

    save moves properly

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,10 @@ var express = require('express')
 // ACTUAL ROUTES:
 router.post('/save_moves', function(req, res) {
   if (!req.user) return res.status(401).send('Please sign in to save moves');
-  User.find(req.user, function(err, user) {
-    if (err) throw err;
-    Move.create({ user_id: user._id, moves: req.body.moves }, function(err, result) {
-      if (err) throw err
-      return res.json(result.toObject())
-    })
+  var move = new Move({ user_id: req.user._id, moves: req.body.moves })
+  move.save(function(err, result) {
+    if (err) throw err
+    return res.json(result.toObject())
   })
 })
 
